5d807b9b59db5a13c0f461c6e44f0f5e82ea4375
Макс Апаков <maksapakov@gmail.com>
2022-09-27 15:16:01 +0300

    ver.2.0

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -20,13 +20,13 @@ public class Main {
         User user2 = new User("Андрей", "Коновалов", (byte) 38);
         User user3 = new User("Владислав", "Пельш", (byte) 46);
 
-//        userDaoHibernate.createUsersTable();
-//        userDaoHibernate.saveUser(user.getName(), user.getLastName(), user.getAge());
-//        userDaoHibernate.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
-//        userDaoHibernate.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
-//        userDaoHibernate.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+        userDaoHibernate.createUsersTable();
+        userDaoHibernate.saveUser(user.getName(), user.getLastName(), user.getAge());
+        userDaoHibernate.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+        userDaoHibernate.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+        userDaoHibernate.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
 
-//        userDaoHibernate.getAllUsers();
+        userDaoHibernate.getAllUsers();
 //        userDaoHibernate.removeUserById(1);
 //        userDaoHibernate.cleanUsersTable();
 //        userDaoHibernate.dropUsersTable();
